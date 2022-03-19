d079aa4b842d903a2b76fe626b926c7f5639de52
Ryan DV <rdevilla@uwaterloo.ca>
2022-03-19 14:52:00 -0400

    create local error space

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
 use libc;
+
 use core::ptr;
+
 use std::env;
 use std::ffi;
+use std::io;
 use std::io::prelude::*;
 use std::net;
-use std::result::Result;
+use std::result;
 
 static DEFAULT_FIBS_SERVER: &str = "fibs.com";
 const DEFAULT_FIBS_PORT: u16 = 4321;
 
-fn resolvev4(hostname: String, port: u16) -> Result<net::SocketAddrV4, String> {
-    let c_hostname = ffi::CString::new(hostname).map_err(|e| e.to_string())?;
-    let c_port = ffi::CString::new(port.to_string()).map_err(|e| e.to_string())?;
+#[derive(Debug)]
+enum Error {
+    IOError(String),
+    MalformedInputError(String),
+    GAIError(String),
+}
+
+type Result<T> = result::Result<T, Error>;
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        Error::IOError(e.to_string())
+    }
+}
+
+impl From<ffi::NulError> for Error {
+    fn from(e: ffi::NulError) -> Error {
+        let nul_pos = e.nul_position();
+        let mut bytes = e.into_vec();
+        bytes.truncate(nul_pos);
+
+        Error::MalformedInputError(
+            format!(
+                "interior nul byte found at position {}, immediately following {}",
+                nul_pos,
+                String::from_utf8_lossy(bytes.as_slice())
+            )
+        )
+    }
+}
+
+impl From<libc::c_int> for Error {
+    fn from(e: libc::c_int) -> Error {
+        unsafe {
+            Error::GAIError(String::from(ffi::CStr::from_ptr(libc::gai_strerror(e)).to_string_lossy()))
+        }
+    }
+}
+
+fn resolvev4(hostname: String, port: u16) -> Result<net::SocketAddrV4> {
+    let c_hostname = ffi::CString::new(hostname)?;
+    let c_port = ffi::CString::new(port.to_string())?;
     let mut res = libc::addrinfo {
         ai_flags: 0,
         ai_family: 0,
@@ -32,12 +74,12 @@ fn resolvev4(hostname: String, port: u16) -> Result<net::SocketAddrV4, String> {
                         (*res_addr).sin_port.swap_bytes(),
                 ))
             }
-            e => Err(e.to_string())
+            e => Err(e.into())
         }
     }
 }
 
-fn main() -> Result<(), String> {
+fn main() -> Result<()> {
     let fibs_hostname = env::vars()
         .find(|(_envar, val)| val == "FIBS_HOSTNAME")
         .map(|(_envar, val)| val)
@@ -50,8 +92,8 @@ fn main() -> Result<(), String> {
     let fibs_addrv4 = resolvev4(fibs_hostname, fibs_port)?;
 
     let mut payload: [u8; 930] = [0; 930];
-    let mut tcp = net::TcpStream::connect(fibs_addrv4).map_err(|e| e.to_string())?;
-    tcp.read_exact(&mut payload).map_err(|e| e.to_string())?;
+    let mut tcp = net::TcpStream::connect(fibs_addrv4)?;
+    tcp.read_exact(&mut payload)?;
     println!("{}", String::from_utf8_lossy(&payload));
 
     Ok(())
